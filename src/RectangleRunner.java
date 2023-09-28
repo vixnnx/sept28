ebcbdee40e065c1324537865b0286a675879e49e
BT_2N2_14 <sample@bths.edu>
2023-09-28 13:34:50 -0400

    finished step 9 of lab

--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class RectangleRunner {
+public class RectangleRunner {
+    public static void main(String[] args) {
+        Rectangle plot1 = new Rectangle(150, 200);
+        Rectangle plot2 = new Rectangle(150);
+        Rectangle plot3 = new Rectangle();
+
+        plot2.setLength(75);
+        plot2.setWidth(75);
+        plot1.setWidth(75);
+        plot3.setWidth(75);
+        System.out.println("These three plots requires "+ (plot1.calculateArea() + plot2.calculateArea() + plot3.calculateArea())+ " square feet of seed");
+    }
 }
